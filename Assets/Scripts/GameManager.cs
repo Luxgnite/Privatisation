9664a32e1b49eede8d14557ada8b81e8e0588890
Luxgnite <ewan.dubois@live.fr>
2020-05-06 08:44:52 +0200

    Flow of Time, Display & Public Services Behavior
    * Time pass at a rate of one month / 4 sec in real life
    * Each month, balance of each public service is revised as follow : 
     - Income increase at a guaranteed rate define by ProfitTendency + a random value betwen -10% and 10%
    - Cost varies by a random value betwen -10% and 10%
    - Privatisation Malus increase by the profit tendency divided by 2
    * Each Public Service have a Private Invest Desirability calculate by the ratio between its balance and 10.000 (the value imagined where Investors begin to feel safe investing)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Globalization;
+
+public class GameManager : MonoBehaviour
+{
+    public Text displayDate;
+    public DateTime date;
+    public int speedTime = 4;
+
+    public int treasury;
+    public List<PublicService> publicServices;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        date = new DateTime(2008,5,1);
+        StartCoroutine(DateUpdate());
+
+        treasury = 0;
+
+        publicServices = new List<PublicService>();
+        publicServices.Add(new PublicService("SNCF", -10000, 1000, 100, 0.4f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DateUpdate()
+    {
+        yield return new WaitForSecondsRealtime(speedTime);
+
+        date = date.AddMonths(1);
+        displayDate.text = date.ToString("y",new CultureInfo("fr-FR"));
+
+        StartCoroutine(DateUpdate());
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+}
---
+++ Assets/Scripts/PublicService.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PublicService : MonoBehaviour
+{
+    string name;
+    int cost;
+    int income;
+    int privMalus;
+    float profitTendency;
+    int privateInvestMinValue = 10000;
+
+    public PublicService(string pName, int baseCost, int baseIncome, int basePrivMalus, float pProfitTendency)
+    {
+        name = pName;
+        cost = baseCost;
+        income = baseIncome;
+        privMalus = basePrivMalus;
+        profitTendency = pProfitTendency;
+    }
+
+    public int Cost
+    {
+        get
+        {
+            return cost;
+        }
+    }
+
+    public int Income
+    {
+        get
+        {
+            return income;
+        }
+    }
+
+    public int PrivMalus
+    {
+        get
+        {
+            return privMalus;
+        }
+    }
+
+    public float ProfitTendency
+    {
+        get
+        {
+            return profitTendency;
+        }
+    }
+
+    public int Balance
+    {
+        get
+        {
+            return income - cost;
+
+        }
+    }
+
+    public float Desirability
+    {
+        get
+        {
+            return Balance/privateInvestMinValue;
+        }
+    }
+
+    public void TimeUpdate()
+    {
+        cost = (int) (cost * (1f + Random.Range(-0.1f, 0.1f)));
+        income = (int)(cost * (1f + profitTendency + Random.Range(-0.1f, 0.1f)));
+        privMalus = (int)(privMalus * (1 + (profitTendency / 2)));
+    }
+}
